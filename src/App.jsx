2c3508deca544b958a3ac61a4232c57b6b94d554
Stuman8818 <djstewart8812@gmail.com>
2024-04-18 16:23:08 -0400

    finish modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
+import Cart from "./components/Cart";
 import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
 
 function App() {
   return (
     <>
+    <UserProgressContextProvider>
       <CartContextProvider>
         <Header />
         <Meals />
+        <Cart />
       </CartContextProvider>
+    </UserProgressContextProvider>
     </>
   );
 }
